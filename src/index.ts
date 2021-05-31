8d5e4f560eca6d2ba08c0abcfbd8ab4aa133cf26
kingbri <bdashore3@gmail.com>
2021-05-31 15:41:55 -0400

    Add method to check if a manga is updated
    This method iterates through the user's titles and sees if there is
    a match within the overall source updates.
    
    A match is declared when:
    1. The title matches
    2. The sources are the same (since the title depends on the source
    name)
    
    If a match exists, further logic is sent. Currently, there are
    dummy console log statements until sending is implemented.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { promises as fs } from 'fs';
+import { MangaPacket } from 'types/sourceEntries';
+import { UserJson } from 'types/userJson';
 import { fetchMangaFox } from './SourceUpdates/MangaFox';
 import { fetchMangaLife } from './SourceUpdates/MangaLife';
 
@@ -8,9 +11,53 @@ if (require.main === module) {
 // Future home for web server
 
 async function main() {
-    // Fetches update json from Manga4Life
-    console.log(await fetchMangaLife());
+    /* Manga entry for testing the filter system
+    const testEntry: MangaPacket = {
+        Name: 'My Hero Academia',
+        Chapter: '125',
+        TimeElapsed: 300,
+        Source: 'MangaLife'
+    };
+    */
 
-    // Fetches update json from MangaFox
-    console.log(await fetchMangaFox());
+    // Empty array of updates from all sources
+    const updateArray: Array<MangaPacket> = [];
+    const userConfig = await fetchUserJson();
+
+    // Fetches updates from Manga4Life
+    const mangaLife = await fetchMangaLife();
+    updateArray.concat(mangaLife);
+
+    // Fetches updates from MangaFox
+    const mangaFox = await fetchMangaFox();
+    updateArray.concat(mangaFox);
+
+    /*
+     * Iterates through each entry in the User configuration.
+     *
+     * If the user entry's title and source is found in the updates list, send a notification.
+     */
+    userConfig.mangas.forEach((userEntry) => {
+        console.log(
+            `Original element title: ${userEntry.title} \nOriginal element source: ${userEntry.source} \n`
+        );
+
+        const result = updateArray.find((updateEntry) => {
+            return userEntry.title === updateEntry.Name && userEntry.source === updateEntry.Source;
+        });
+
+        if (result === undefined) {
+            console.log(`No updates found for ${userEntry.title}`);
+        } else {
+            console.log(`Sending chapter: ${result.Chapter} of ${result.Name}`);
+        }
+    });
+}
+
+// Fetches user configuration.
+async function fetchUserJson(): Promise<UserJson> {
+    const file = await fs.readFile('backupDump/encodedManga.json', 'utf8');
+    const userJson = JSON.parse(file);
+
+    return userJson;
 }
