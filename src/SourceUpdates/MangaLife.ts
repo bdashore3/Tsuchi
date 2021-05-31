dc23f7e3db4404c6180537471c2845e0d55a7a1c
kingbri <bdashore3@gmail.com>
2021-05-31 11:54:56 -0400

    MangaLife: Adapt to MangaPacket object
    We will be using a common object to send updates, make MangaLife
    adapt to this schema.
    
    In addition, truncate the source list to a maximum of 20 and truncates
    if a time is less than 400 minutes since we'll know that the ahead
    entries are old updates.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/SourceUpdates/MangaLife.ts
+++ src/SourceUpdates/MangaLife.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
-import { MangaLifeEntry } from 'types/sourceEntries';
+import { MangaLifeEntry, MangaPacket } from 'types/sourceEntries';
 
-export async function fetchMangaLife(): Promise<Array<MangaLifeEntry>> {
+export async function fetchMangaLife(): Promise<Array<MangaPacket>> {
+    const mangaLifeUpdates: Array<MangaPacket> = [];
+
     const baseDomain = 'https://manga4life.com/';
 
     const html = await axios.get(baseDomain);
@@ -13,9 +15,10 @@ export async function fetchMangaLife(): Promise<Array<MangaLifeEntry>> {
 
     const child = $('script').get()[16].children[0];
 
+    /* eslint-disable @typescript-eslint/no-explicit-any */
     const data: string = (<any>child).data;
 
-    const mangaLifeUpdates: Array<MangaLifeEntry> = JSON.parse(
+    const rawUpdates: Array<MangaLifeEntry> = JSON.parse(
         data
             .substring(
                 data.lastIndexOf('vm.LatestJSON = [{') + 16,
@@ -23,6 +26,43 @@ export async function fetchMangaLife(): Promise<Array<MangaLifeEntry>> {
             )
             .slice(0, -5)
     );
+    rawUpdates.splice(20, rawUpdates.length - 1);
+
+    rawUpdates.forEach((element) => {
+        const timeElapsed = convertTime(element.Date);
+
+        if (timeElapsed > 400) {
+            return;
+        }
+
+        const mangaPacket: MangaPacket = {
+            Chapter: parseChapter(element.Chapter),
+            Name: element.SeriesName,
+            Source: 'MangaLife',
+            TimeElapsed: timeElapsed
+        };
+
+        mangaLifeUpdates.push(mangaPacket);
+    });
 
     return mangaLifeUpdates;
+}
+
+function convertTime(inputDate: string): number {
+    const currentDate = new Date();
+    const stampedDate = Date.parse(inputDate);
+    const minutes = currentDate.getTime() / 60000 - stampedDate / 60000;
+
+    return minutes;
+}
+
+function parseChapter(inputChapter: string): string {
+    const numbers = inputChapter.split('');
+    numbers.shift();
+
+    if (parseInt(numbers[numbers.length - 1]) > 0) {
+        numbers[numbers.length - 1] = `.${numbers[numbers.length - 1]}`;
+    }
+
+    return numbers.join('').replace(/0/g, '');
 }
