f2f71f056afa46a25dc479b8d441e3cc004b7295
Naman Pandey <github@pandeynaman.com>
2021-06-01 00:51:55 +0530

    Create iftt.ts

---
+++ src/NotificationHandler/iftt.ts
@@ -0,0 +1,13 @@
+import { MangaPacket } from 'types/sourceEntries';
+import axios from 'axios';
+
+
+export class Ifttt {
+
+    constructor(private key: string, private value: string) {}
+
+    async sendNotification(mangapacket: MangaPacket): Promise<void> {
+
+        var a;
+    }
+}
