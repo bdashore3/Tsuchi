10fb57983b9e3d822e68538f2cf6147123aafdbe
pandeynmn <github@pandeynaman.com>
2021-06-01 17:54:38 +0530

    Update MangaDex.ts

--- src/SourceUpdates/MangaDex.ts
+++ src/SourceUpdates/MangaDex.ts
@@ -28,7 +28,6 @@ export async function fetchMangaDex(): Promise<Array<MangaPacket>> {
                     Source: 'MangaDex'
                 };
                 MangaDexUpdates.push(mangapacket);
-
             }
         }
     )
