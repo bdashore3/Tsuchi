eea6dd503b1b2e35e36936783cfd812d8c1d899a
kingbri <bdashore3@gmail.com>
2021-06-05 22:14:52 -0400

    Utils: Use trimEnd() instead of trim()
    trim removes the first character from each string for a weird reason.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -19,7 +19,7 @@ export function removeExtraChars(title: string): string {
         .replaceAll("'", '')
         .replace(puncRegex, ' ')
         .replace(spaceRegex, ' ')
-        .trim();
+        .trimEnd();
 
     return strippedString;
 }
