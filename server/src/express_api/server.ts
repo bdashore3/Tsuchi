9e0e5f3fb3d4fe544ebb9e2df6d826a2748d80c7
kingbri <bdashore3@gmail.com>
2021-08-14 13:02:09 -0400

    API: Make all requests use /api by default
    The URL scheme for triggering the api is /api/sub/paths/here.
    
    Make an express router append /api by default to save development
    speed when making new request catchers.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- server/src/express_api/server.ts
+++ server/src/express_api/server.ts
@@ -1,34 +1,35 @@
-// node server/dist/express_api/server.js
+// To run: node server/dist/express_api/server.js
+// API will send JSON responses to the frontend website/app
 
 import express from 'express';
 
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.get('/api', (req, res) => {
+app.use('/api', router);
+
+router.get('/', (req, res) => {
     res.send('API Base Path');
 });
 
-app.get('/api/authenticate/:username/:password', (req, res) => {
+router.get('/authenticate/:username/:password', (req, res) => {
     // Hash Password and check hash ...
 
     res.send(`Username: ${req.params.username}<br>Password: ${req.params.password}`);
 });
 
-app.post('/api/authenticate', (req, res) => {
+router.post('/authenticate', (req, res) => {
     // post_body is a json obj -> {username:u, password:p}
     const post_body = req.body;
     console.log(post_body);
     res.send('Auth for POST.');
 });
 
-app.get('/*', (req, res) => {
+router.get('/*', (req, res) => {
     res.send("<html><body><img src='https://http.cat/404'></body></html>");
 });
 
-app.listen(3000, () => console.log(`HTTP Server running on port 3000`));
+app.listen(3000, () => console.log(`Tsuchi API running on port 3000`));
 
 // // runs the https, no need to change stuff here
 // const httpServer = http.createServer(app);
